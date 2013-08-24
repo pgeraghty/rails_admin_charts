29676ad08da1355a39073fba9ff085793bf039fb
Paul Geraghty <pgeraghty07@gmail.com>
2013-08-24 03:17:25 +0200

    Hash access using Date for MySQL with String fallback for SQLite

--- lib/rails_admin_charts.rb
+++ lib/rails_admin_charts.rb
@@ -9,12 +9,13 @@ module RailsAdminCharts
   module ClassMethods
     def total_records_since(since = 30.days.ago)
       totals, before_count = self.group('DATE(created_at)').count, self.where('created_at < ?', since.to_date).count
-      (since.to_date..Date.today).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day.to_s] || 0) }
+      # TODO separate MySQL/Postgres approaches using ActiveRecord::Base.connection.adapter_name or check hash key is_a? String/Date
+      (since.to_date..Date.today).each_with_object([]) { |day, a| a << (a.last || before_count) + (totals[day] || totals[day.to_s] || 0) }
     end
 
     def delta_records_since(since = 30.days.ago)
       deltas = self.group('DATE(created_at)').count
-      (since.to_date..Date.today).map { |date| deltas[date.to_s] || 0 }
+      (since.to_date..Date.today).map { |date| deltas[date] || deltas[date.to_s] || 0 }
     end
 
     def graph_data(since=30.days.ago)
